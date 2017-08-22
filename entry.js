6531440ec25b8ae270588debcc2f5c7a13b8d23c
陈宏恩 <251601797@qq.com>
2017-08-22 10:52:51 +0800

    init

---
+++ entry.js
@@ -0,0 +1,25 @@
+/**
+ * CANNOT use `import` to import `es5-shim`,
+ * because `import` will be transformed to `Object.defineProperty` by babel,
+ * `Object.defineProperty` doesn't exists in IE8,
+ * (but will be polyfilled after `require('es5-shim')` executed).
+ */
+
+require('es5-shim');
+require('es5-shim/es5-sham');
+require('console-polyfill');
+
+/**
+ * CANNOT use `import` to import `react` or `react-dom`,
+ * because `import` will run `react` before `require('es5-shim')`.
+ */
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello World</h1>,
+  document.getElementById('app')
+);
