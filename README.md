91ef307d0aeb144b351e8427ecbdc51f3ab0b1c7
陈宏恩 <251601797@qq.com>
2017-08-24 11:37:47 +0800

    link

--- README.md
+++ README.md
@@ -45,8 +45,9 @@ npm install babel-polyfill --save-dev（需参照npm官方配置）
 
 polyfill的大意就是扩展ie8等低版本浏览器中所不具备的方法。如foreach/keys等。
 
-7. router link
-this.context.history is undefined
+# 令人窒息的八阿哥
+1. router - <Link>
+当在entry.js入口文件写主页<HomePage>，并在其中写<Link>时，抛出“this.context.history is undefined”，将<HomePage>迁移至单独文件解决。
 
 
 
--- entry.js
+++ entry.js
@@ -19,89 +19,21 @@ require('es5-shim/es5-sham');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-import { Menu, Breadcrumb, Icon } from 'antd';
-require('./style/test.css');
-const SubMenu = Menu.SubMenu;
-
 const {Home} = require('./component/module');
 const {User} = require('./component/user');
+const {HomePage} = require('./component/App');
 
 //import { IndexRoute } from 'react-router'
-const{ Router, Route, IndexRoute, Link } = require('react-router');
+const{ Router, Route, IndexRoute } = require('react-router');
 const { render } = ReactDOM;
 
-const Frame = React.createClass({
-  getInitialState() {
-    return {
-      collapse: true,
-    };
-  },
-  onCollapseChange() {
-    this.setState({
-      collapse: !this.state.collapse,
-    })
-  },
-  render() {
-    const collapse = this.state.collapse;
-    return (
-      <div className={collapse ? "ant-layout-aside ant-layout-aside-collapse" : "ant-layout-aside"}>
-        <aside className="ant-layout-sider">
-          <div className="ant-layout-logo"></div>
-          <Menu mode="inline" theme="dark" defaultSelectedKeys={['user']}>
-            <Menu.Item key="user">
-              <Icon type="user" /><span className="nav-text">导航一</span>
-            </Menu.Item>
-            <Menu.Item key="setting">
-              <Icon type="setting" /><span className="nav-text">导航二</span>
-            </Menu.Item>
-            <Menu.Item key="laptop">
-              <Icon type="laptop" /><span className="nav-text">导航三</span>
-            </Menu.Item>
-            <Menu.Item key="notification">
-              <Icon type="notification" /><span className="nav-text">导航四</span>
-            </Menu.Item>
-            <Menu.Item key="folder">
-              <Icon type="folder" /><span className="nav-text">导航五</span>
-            </Menu.Item>
-          </Menu>
-          <div className="ant-aside-action" onClick={this.onCollapseChange}>
-            {collapse ? <Icon type="right" /> : <Icon type="left" />}
-          </div>
-        </aside>
-        <div className="ant-layout-main">
-          <div className="ant-layout-header"></div>
-          <div className="ant-layout-breadcrumb">
-            <Breadcrumb>
-              <Breadcrumb.Item>首页</Breadcrumb.Item>
-              <Breadcrumb.Item>应用列表</Breadcrumb.Item>
-              <Breadcrumb.Item>某应用</Breadcrumb.Item>
-            </Breadcrumb>
-          </div>
-          <div className="ant-layout-container">
-            <div className="ant-layout-content">
-              <div style={{ height: 220 }}>
-                {this.props.children}
-              </div>
-            </div>
-          </div>
-          {/* <div className="ant-layout-footer">
-           Ant Design 版权所有 © 2015 由蚂蚁金服体验技术部支持
-           </div>*/}
-        </div>
-      </div>
-    );
-  },
-});
-
 ReactDOM.render((
-    <Frame>
       <Router>
-        <Route path="/" component={Home}>
+        <Route path="/" component={HomePage}>
           <Route path="home" component={Home} />
           <Route path="user" component={User} />
         </Route>
       </Router>
-    </Frame>
   ),
   document.getElementById('app')
 );
